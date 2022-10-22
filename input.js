3b75b2c8b7a7fb1c6b6425bd37e7f541c1659ef4
Shelly <shellyb3554@gmail.com>
2022-10-22 15:39:11 -0400

    input

--- input.js
+++ input.js
@@ -1,2 +1,11 @@
 
+var input;
+document.getElementById('userInput').onkeydown =function(event){
+    if(event.keyCode ==13){
+    returnText();
+    }
+}
 
+function returnText(){
+    let input = document.ElementById("userInput").value;
+}

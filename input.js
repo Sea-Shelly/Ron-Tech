0b33396bf94ecdea7f77d59c0c0548269072b55c
Shelly <shellyb3554@gmail.com>
2022-10-24 13:10:26 -0400

    uhh

--- input.js
+++ input.js
@@ -1,9 +1,16 @@
 
 var input;
-document.getElementById('userInput').onkeydown =function(event){
+document.getElementById("userInput").onKeyDown =function(event){
     if(event.keyCode ==13){
     returnText();
     }
+}
+
+while(event.keyCode != 13){
+if(input.toLowerCase() == "new"){
+window.location.replace("www.youtube.com");;
+} else if(input.toLowerCase() == "username"){
+}
 }
 
 function returnText(){

4e1b6854476be9e2093f8e1e3cf91d535872fd25
Sea-Shelly <93165734+Sea-Shelly@users.noreply.github.com>
2022-10-27 10:08:12 -0400

    janky

--- input.js
+++ input.js
@@ -8,7 +8,7 @@ var phrase;
 document.getElementById("userInput").addEventListener("input", getInput());
 
 
-function getInput(){
+function getInput(event){
     if(event.code("Enter")){
    let phrase = document.getElementById("userInput");
    alert("The entered button has been pressed");

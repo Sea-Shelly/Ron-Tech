909ee00e835dbb0523abc63d59191af39eac5b2c
Shelly <shellyb3554@gmail.com>
2022-10-31 16:09:43 -0400

    🙏🏻

--- input.js
+++ input.js
@@ -1,13 +1,6 @@
-
-
-
-var input;
-
-input.addEventListener("push", getInput);
-
-function getInput(){
-    if(event.code("Enter")){
-   let input = document.getElementById("userInput");
-   alert("The entered button has been pressed");
-   }
-}
+document.getElementById("userInput").addEventListener("keypress", function (event) {
+    if (event.code === "Enter") {
+        let input = document.getElementById("userInput").value
+        alert(input)
+    }
+})

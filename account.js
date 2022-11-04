63d9bf63dc898838eb5a89043a90fc5ddae817ca
Sea-Shelly <93165734+Sea-Shelly@users.noreply.github.com>
2022-11-04 12:51:59 -0400

    page

--- account.js
+++ account.js
@@ -1,11 +1,25 @@
 
+
+let x = 0;
+
 document.getElementById("newAccount").addEventListener("keypress", function (event) {
     if (event.code === "Enter") {
-        let last = document.getElementById("newAccount").value
+
+
+
+        switch(x){
+        case 0:
+         let last = document.getElementById("newAccount").value
+         break;
+        case 1:
         document.getElementById("names").innerHTML = "Please enter your first name";
-        let first = document.getElementById("newAccount").value;
-        alert(first + last);
+                let first = document.getElementById("newAccount").value;
+                alert(first + last);
+                break;
+
+        }
 
 
     }
+    x++;
 })
--- input.js
+++ input.js
@@ -1,10 +1,13 @@
 document.getElementById("userInput").addEventListener("keypress", function (event) {
     if (event.code === "Enter") {
         let input = document.getElementById("userInput").value
+
         if(input == "new"){
          window.location.href = "makiingAccount.html";
         } else if(input == "login"){
-
+            document.getElementById("indexP").innerHtml = "Enter your ID:";
+            document.getElementById("onePart").innerHtml = "";
+            document.getElementById("theOtherPart").innerHtml = "";
         } else{
 
         }
